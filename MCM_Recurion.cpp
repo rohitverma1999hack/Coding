6ecd2e6df9022837ee9ad8d74f1114e2a8412b5f
Rohit Verma <71862247+bhosafi@users.noreply.github.com>
2020-10-01 09:46:20 +0530

    Update MCM_Recurion.cpp

--- MCM_Recurion.cpp
+++ MCM_Recurion.cpp
@@ -5,15 +5,16 @@
 using namespace std;
 
 int MCM(int arr[],int i,int j){
+	//Base Condition
 	if(i >= j) return 0;
 
 	int mn=INT_MAX;
-
+	
+	//Hypothesis
 	for(int k=i;k<j;k++){
 		int temp = MCM(arr,i,k) + MCM(arr,k+1,j) + arr[i-1] * arr[k] * arr[j];
 		mn=min(mn,temp);
 	}
-
 	return mn;
 
 }
